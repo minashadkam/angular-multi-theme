7cefcc8ee05a06fd7e8d1fbf1777d9bdc907ba09
m_shadkam <m_shadkam@ISC.CO.IR>
2024-07-27 15:55:27 +0330

    dynamically inject library theme files in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,58 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {SharedComponent} from "../../projects/shared/src/lib/shared.component";
+import {Theme} from "./theme/theme";
+import {ThemeService} from "./theme/theme.service";
+import {iterator} from "rxjs/internal/symbol/iterator";
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedComponent],
+  imports: [CommonModule, RouterOutlet, SharedComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent  implements OnInit{
+export class AppComponent implements OnInit {
   title = 'angular-multi-theme';
+  themeArr: Theme[];
+  selectedTheme: Theme = Theme.SECONDARY;
 
+  constructor(
+    private themeService: ThemeService,
+    private renderer2: Renderer2,
+  ) {
+    this.themeArr = [Theme.GREEN_ORANGE, Theme.SECONDARY, Theme.DARK_YELLOW];
+    console.log('themeArr => ', this.themeArr);
+  }
 
+  ngOnInit(): void {
+    this.initialize();
 
-  ngOnInit() {
-    const darkMode = localStorage.getItem("theme") == 'dark' || window.matchMedia("(prefers-color-scheme: dark)")?.matches;
-    const newTheme = darkMode ? 'dark' : 'light';
-
-    localStorage.setItem("theme", newTheme);
-    document.querySelector("html")!.setAttribute("data-theme", newTheme);
+    // const darkMode = localStorage.getItem("theme") == 'dark' || window.matchMedia("(prefers-color-scheme: dark)")?.matches;
+    // const newTheme = darkMode ? 'dark' : 'light';
+    // localStorage.setItem("theme", newTheme);
+    // document.querySelector("html")!.setAttribute("data-theme", newTheme);
     // document.body.classList.toggle(newTheme);
 
   }
 
+  initialize() {
+    this.themeService.setTheme(this.selectedTheme, this.renderer2);
+  }
+
+  chooseTheme(theme: Theme) {
+    debugger
+    this.themeService.setTheme(Theme.DARK_YELLOW, this.renderer2);
+  }
 
   changeTheme() {
-    const newTheme =  localStorage.getItem("theme") == 'dark' ? 'light' : 'dark';
+    const newTheme = localStorage.getItem("theme") == 'dark' ? 'light' : 'dark';
     localStorage.setItem("theme", newTheme);
     document.querySelector("html")!.setAttribute("data-theme", newTheme);
     // document.body.classList.toggle(newTheme);
   };
+
+  protected readonly iterator = iterator;
 }
---
+++ src/app/theme/theme.ts
@@ -0,0 +1,5 @@
+export enum Theme {
+  DARK_YELLOW = 'dark-yellow',
+  GREEN_ORANGE = 'green-orange',
+  SECONDARY = 'secondary',
+  }
